d7c46a9bf06953ea775b534c93d5c5465a3debf5
Micahel Turner <aztex87068@juno.com>
2015-10-13 11:19:37 -0600

    first html experience.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+	<html lang=""eng">
+<head>
+	<meta charset=""utf-8/>
+	<title>about michael</title
+</head>
+<body>
+	<h1>michael turner</h1>
+	<p>
+		deep dive coding student,single dad,cooks competitive BBQ,avid cook, working on level 1 sommelier rating
+
+	</p>
+</body>
